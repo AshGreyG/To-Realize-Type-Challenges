eec204ef8605f21088cea4f67b3b3c6e25374c7f
AshGreyG <ashgrey.huaier@gmail.com>
2025-03-26 12:41:16 +0800

    :tada: first commit with solution of 0002-medium-return-type

---
+++ questions/0002-medium-return-type.ts
@@ -0,0 +1,12 @@
+// #medium #infer #built-in
+
+type GetReturnType<F extends Function>
+  = F extends (...params: any[]) => infer Return
+    ? Return
+    : never;
+
+const fn = (v: boolean) => {
+  return v ? 1 : 2;
+}
+
+type Res = GetReturnType<typeof fn>;  // 1 | 2
